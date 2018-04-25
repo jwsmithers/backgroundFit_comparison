9a42591a7dd237bdee6c2f20a4991f427e9b3014
jwsmith <joshua.wyatt.smith@cern.ch>
2018-04-25 22:49:33 +0200

    add a statonly ratio plot for SL

--- compare.py
+++ compare.py
@@ -2,12 +2,15 @@ import ROOT
 from ROOT import gROOT
 gROOT.SetBatch(True)
 
+SL_path_stat_only="/eos/user/j/jwsmith/ttgamma/plottingPipeline/v010/11_04_2018/singlelepton_fullFit_merged_C_Off_StatOnly/build/SR1_ejets_mujets_merged/ejets_mujets_merged/Histograms/"
 SL_path="/eos/user/j/jwsmith/ttgamma/plottingPipeline/v010/11_04_2018/singlelepton_fullFit_merged_C_Off/build/SR1_ejets_mujets_merged/ejets_mujets_merged/Histograms/"
 DL_path="/eos/user/j/jwsmith/ttgamma/plottingPipeline/v010/11_04_2018/dilepton_fullFit_merged_C_Off/build/SR1_ee_mumu_emu_merged/ee_mumu_emu_merged/Histograms/"
+DL_path_stat_only="/eos/user/j/jwsmith/ttgamma/plottingPipeline/v010/11_04_2018/dilepton_fullFit_merged_C_Off_StatOnly/build/SR1_ee_mumu_emu_merged/ee_mumu_emu_merged/Histograms/"
 
-def plot(channel,ntuplePath,variable,prefitNtuple,xvar):
+def plot(channel,ntuplePath,ntuplePath_statOnly,variable,prefitNtuple,xvar):
   prefit=ROOT.TFile(ntuplePath+prefitNtuple,"r")
   postfit=ROOT.TFile(ntuplePath+variable+"_postFit.root","r")
+  postfit_stat_only=ROOT.TFile(ntuplePath_statOnly+variable+"_postFit.root","r")
 
   c1 = ROOT.TCanvas("canvas","",0,0,800,800);
   c1.SetFillColor(0);
@@ -55,33 +58,57 @@ def plot(channel,ntuplePath,variable,prefitNtuple,xvar):
   xaxis.SetLabelSize(0)
   yaxis.SetTitle("Events")
   yaxis.SetTitleOffset(1.7)
+
+  Max=hfake.GetMaximum()
+  if channel=="SL":
+    hfake.SetMaximum(Max+220)
+  else:
+    hfake.SetMaximum(Max+5)
   hfake.Draw("H")
 
   ####### Now get post-fit
   hfake_post = postfit.Get("h_hadronfakes_postFit")
   efake_post = postfit.Get("h_electronfakes_postFit")
   other_post = postfit.Get("h_Other_postFit")
-
   hfake_post.Add(efake_post)
   hfake_post.Add(other_post)
-
   if channel=="SL":
     wphoton_post = postfit.Get("h_Wphoton_postFit")
     qcd_post = postfit.Get("h_QCD_postFit")
-
     hfake_post.Add(wphoton_post)
     hfake_post.Add(qcd_post)
-
   if channel=="DL":
     zphoton_post = postfit.Get("h_Zphoton_postFit")
     hfake_post.Add(zphoton_post)
-
   hfake_post.SetFillColor(ROOT.kBlue);
   hfake_post.SetFillStyle(3315)
   hfake_post.SetMarkerStyle(0)
   hfake_post.SetLineWidth(3)
   hfake_post.SetLineColor(ROOT.kBlue)
   hfake_post.Draw("H same")
+
+  # Stat only SL
+  hfake_post_stat_only = postfit_stat_only.Get("h_hadronfakes_postFit")
+  efake_post_stat_only = postfit_stat_only.Get("h_electronfakes_postFit")
+  other_post_stat_only = postfit_stat_only.Get("h_Other_postFit")
+  hfake_post_stat_only.Add(efake_post_stat_only)
+  hfake_post_stat_only.Add(other_post_stat_only)
+  if channel=="SL":
+    wphoton_post_stat_only = postfit_stat_only.Get("h_Wphoton_postFit")
+    qcd_post_stat_only = postfit_stat_only.Get("h_QCD_postFit")
+    hfake_post_stat_only.Add(wphoton_post_stat_only)
+    hfake_post_stat_only.Add(qcd_post_stat_only)
+  if channel=="DL":
+    zphoton_post_stat_only = postfit_stat_only.Get("h_Zphoton_postFit")
+    hfake_post_stat_only.Add(zphoton_post_stat_only)
+
+  hfake_post_stat_only.SetFillColor(ROOT.kRed);
+  hfake_post_stat_only.SetFillStyle(3395)
+  hfake_post_stat_only.SetMarkerStyle(0)
+  hfake_post_stat_only.SetLineWidth(3)
+  hfake_post_stat_only.SetLineColor(ROOT.kRed)
+  if channel=="SL":
+    hfake_post_stat_only.Draw("H same")
 
   lumi = ROOT.TLatex();
   lumi.SetNDC();
@@ -97,6 +124,8 @@ def plot(channel,ntuplePath,variable,prefitNtuple,xvar):
   leg = ROOT.TLegend(0.4,0.6,0.55,0.73);
   leg.SetTextSize(0.03);
   leg.AddEntry(hfake,"Pre-fit background","l");
+  if channel=="SL":
+    leg.AddEntry(hfake_post_stat_only,"Post-fit background, StatOnly","l");
   leg.AddEntry(hfake_post,"Post-fit background","l");
   leg.SetBorderSize(0)
   leg.SetFillStyle(0)
@@ -127,6 +156,13 @@ def plot(channel,ntuplePath,variable,prefitNtuple,xvar):
   x.SetLabelSize(21)
   ratio1.SetLineColor(ROOT.kBlue)
   ratio1.Draw()
+
+  if channel=="SL":
+    ratio2 = hfake.Clone("ratio_2")
+    ratio2.Divide(hfake_post_stat_only)
+    ratio2.SetLineColor(ROOT.kRed)
+    ratio2.Draw("same")
+
   line = ROOT.TF1("Sig_fa1","1",-1000,1000);
   line.Draw("same")
   line.SetLineColor(ROOT.kGreen+1);
@@ -135,15 +171,15 @@ def plot(channel,ntuplePath,variable,prefitNtuple,xvar):
 
 
 
-plot("SL",SL_path, "event_ELD_MVA_ejets","ejets_mujets_merged_event_ELD_MVA_ejets_histos.root","event level descriminator")
-plot("SL",SL_path, "ph_pt_ejets","ejets_mujets_merged_ph_pt_ejets_histos.root","ph_pt")
-plot("SL",SL_path, "ph_eta_ejets","ejets_mujets_merged_ph_eta_ejets_histos.root","ph_eta")
-plot("SL",SL_path, "dR_lept_ejets","ejets_mujets_merged_dR_lept_ejets_histos.root","#DeltaR(#gamma,l)")
+plot("SL",SL_path,SL_path_stat_only, "event_ELD_MVA_ejets","ejets_mujets_merged_event_ELD_MVA_ejets_histos.root","event level descriminator")
+plot("SL",SL_path,SL_path_stat_only, "ph_pt_ejets","ejets_mujets_merged_ph_pt_ejets_histos.root","p_{T}(#gamma)")
+plot("SL",SL_path,SL_path_stat_only, "ph_eta_ejets","ejets_mujets_merged_ph_eta_ejets_histos.root","#eta(#gamma)")
+plot("SL",SL_path,SL_path_stat_only, "dR_lept_ejets","ejets_mujets_merged_dR_lept_ejets_histos.root","#DeltaR(#gamma,l)")
 
-plot("DL",DL_path, "dEta_lep_ee","ee_mumu_emu_merged_dEta_lep_ee_histos.root","#Delta#eta(l,l)")
-plot("DL",DL_path, "dPhi_lep_ee","ee_mumu_emu_merged_dPhi_lep_ee_histos.root","#Delta#phi(l,l)")
-plot("DL",DL_path, "dR_lept_ee","ee_mumu_emu_merged_dR_lept_ee_histos.root","#DeltaR(#gamma,l)")
-plot("DL",DL_path, "event_ELD_MVA_ee","ee_mumu_emu_merged_event_ELD_MVA_ee_histos.root","event level descriminator")
-plot("DL",DL_path, "ph_pt_ee","ee_mumu_emu_merged_ph_pt_ee_histos.root","ph_pt")
-plot("DL",DL_path, "ph_eta_ee","ee_mumu_emu_merged_ph_eta_ee_histos.root","#eta(#gamma)")
+plot("DL",DL_path,DL_path_stat_only, "dEta_lep_ee","ee_mumu_emu_merged_dEta_lep_ee_histos.root","#Delta#eta(l,l)")
+plot("DL",DL_path,DL_path_stat_only, "dPhi_lep_ee","ee_mumu_emu_merged_dPhi_lep_ee_histos.root","#Delta#phi(l,l)")
+plot("DL",DL_path,DL_path_stat_only, "dR_lept_ee","ee_mumu_emu_merged_dR_lept_ee_histos.root","#DeltaR(#gamma,l)")
+plot("DL",DL_path,DL_path_stat_only, "event_ELD_MVA_ee","ee_mumu_emu_merged_event_ELD_MVA_ee_histos.root","event level descriminator")
+plot("DL",DL_path,DL_path_stat_only, "ph_pt_ee","ee_mumu_emu_merged_ph_pt_ee_histos.root","p_{T}(#gamma)")
+plot("DL",DL_path,DL_path_stat_only, "ph_eta_ee","ee_mumu_emu_merged_ph_eta_ee_histos.root","#eta(#gamma)")
 
